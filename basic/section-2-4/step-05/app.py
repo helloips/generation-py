d879d5017231157decf7f2248de4fbf1d56849f7
Pavel Ivanov <p.ivanov@hh.ru>
2023-09-14 17:49:00 +0300

    added section № 2.4

---
+++ basic/section-2-4/step-05/app.py
@@ -0,0 +1,2 @@
+number = int(input())
+print(number, number + 1, number + 2, sep='\n')
---
+++ basic/section-2-4/step-10/app.py
@@ -0,0 +1,6 @@
+monitor_cost = int(input())
+system_unit_cost = int(input())
+keyboard_cost = int(input())
+mouse_cost = int(input())
+total_pc_cost = monitor_cost + system_unit_cost + keyboard_cost + mouse_cost
+print(3 * total_pc_cost)
