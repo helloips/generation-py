cb29fc60544bcf42b577e18238ee7da6633d7e26
Pavel Ivanov <p.ivanov@hh.ru>
2023-09-15 12:49:04 +0300

    added section № 3.2

---
+++ basic/section-3-2/step-02/app.py
@@ -0,0 +1,6 @@
+# https://stepik.org/lesson/291000/step/2?unit=272483
+
+a = int(input())
+b = int(input())
+print('Квадрат суммы', a, 'и', b, 'равен', (a + b)**2)
+print('Сумма квадратов', a, 'и', b, 'равна', a**2 + b**2)
