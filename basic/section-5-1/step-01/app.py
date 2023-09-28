3a0f08ca650ff44df4cae2b733ed0c08d1a83429
Pavel Ivanov <p.ivanov@hh.ru>
2023-09-28 16:18:52 +0300

    added section № 5.1

---
+++ basic/section-5-1/step-01/app.py
@@ -0,0 +1,8 @@
+# https://stepik.org/lesson/292172/step/1?unit=273659
+
+year = int(input())
+
+if year % 100 == 0:
+    print('YES')
+else:
+    print('NO')
---
+++ basic/section-5-1/step-04/app.py
@@ -0,0 +1,26 @@
+# https://stepik.org/lesson/292172/step/4?unit=273659
+
+number = int(input())
+
+if number == 1:
+    print('I')
+elif number == 2:
+    print('II')
+elif number == 3:
+    print('III')
+elif number == 4:
+    print('IV')
+elif number == 5:
+    print('V')
+elif number == 6:
+    print('VI')
+elif number == 7:
+    print('VII')
+elif number == 8:
+    print('VIII')
+elif number == 9:
+    print('IX')
+elif number == 10:
+    print('X')
+else:
+    print('ошибка')
