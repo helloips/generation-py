e3b9d8c760f6bb559a6699a3d007f0b85cca76c8
Pavel Ivanov <p.ivanov@hh.ru>
2023-10-03 12:54:35 +0300

    added section № 6.2 - 6.3

---
+++ basic/section-6-2/step-13/app.py
@@ -0,0 +1,8 @@
+# https://stepik.org/lesson/265115/step/13?unit=246063
+
+str_1 = input()
+
+if 'суббота' in str_1 or 'воскресенье' in str_1:
+    print('YES')
+else:
+    print('NO')
---
+++ basic/section-6-3/step-02/app.py
@@ -0,0 +1,12 @@
+# https://stepik.org/lesson/265110/step/2?unit=246058
+
+from math import sqrt
+
+x_1 = float(input())
+y_1 = float(input())
+x_2 = float(input())
+y_2 = float(input())
+
+p = sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
+
+print(p)
