eb5576bf21bd7edd837661605223e52f5bf80e8f
Pavel Ivanov <p.ivanov@hh.ru>
2023-12-29 11:24:30 +0300

    added lesson 13.1

---
+++ basic/src/lesson-13-01/step-08.py
@@ -0,0 +1,12 @@
+# https://stepik.org/lesson/331750/step/8?unit=315129
+
+# объявление функции
+def draw_box():
+    print('*' * 10)
+    for _ in range(12):
+        print('*        *')
+    print('*' * 10)
+
+
+# основная программа
+draw_box()  # вызов функции
---
+++ basic/src/lesson-13-01/step-09.py
@@ -0,0 +1,10 @@
+# https://stepik.org/lesson/331750/step/9?unit=315129
+
+# объявление функции
+def draw_triangle():
+    for i in range(1, 11):
+        print('*' * i)
+
+
+# основная программа
+draw_triangle()  # вызов функции
